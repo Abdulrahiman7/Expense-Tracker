c3e6fa5fef9a864c7d17d7302da749bf2562f63e
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-09-06 09:01:55 +0530

    password encryption

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,20 +1,31 @@
 const User= require('../model/user');
-
+const bcrypt = require('bcrypt');
 exports.createUser= async (req,res,next) => {
     try{
-        if(req.body.name =='' || req.body.email =='' || req.body.password =='')
-        {
-            res.status(204).json(null);
-        }else{
-        const find=User.findAll({where:{'email': req.body.email}})
-        if(find[0]==null)
-        {
-            res.status(208).json(find[0]);
-        }else{
-        const user=User.create(req.body)
-        res.status(200).json(user);
-        }
-        }
+        const name = req.body.name;
+const email = req.body.email;
+const password = req.body.password;
+
+if (name === '' || email === '' || password === '') {
+    res.status(400).json({ message: 'Incomplete request data' });
+} else {
+    const find = await User.findAll({ where: { 'email': email } });
+    if (find.length > 0) {
+        res.status(409).json({ message: 'User with this email already exists' });
+    } else {
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if (err) {
+                // Handle bcrypt hashing error
+                console.error('Error hashing password:', err);
+                res.status(500).json({ message: 'Internal server error' });
+            } else {
+                const user = await User.create({ name, email, password: hash });
+                res.status(200).json(user);
+            }
+        });
+    }
+}
+
     }
     catch(err)
     {
