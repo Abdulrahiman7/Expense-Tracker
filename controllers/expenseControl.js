388e66e25a8c676af111f28df62e23a1831ea055
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-09-15 20:06:12 +0530

    transaction implementation for post and delete

--- controllers/expenseControl.js
+++ controllers/expenseControl.js
@@ -1,17 +1,26 @@
-const Expense=require('../model/expenseModel')
+
+const Expense=require('../model/expenseModel');
+const sequelize=require('../util/database');
 
 exports.postExpense=async (req, res, next)=> {
+    const t=await sequelize.transaction();
     try
     {
+        
         const exp=req.body;
-        const x=await req.user.createExpense(exp);
-        console.log(exp.amount);
+        const x=await req.user.createExpense(exp,{transaction:t});
         req.user.totalExpense += +exp.amount;
-        await req.user.save();
+        await req.user.save({transaction:t});
+
+        t.commit();
         res.status(200).json(x[0]);
     }
     catch(err)
     {
+        if(t)
+        {
+            t.rollback();
+        }
         console.log(err);
     }
 }
@@ -29,16 +38,23 @@ exports.getExpense=async (req, res, next)=>{
 }
 
 exports.deleteExpense= async (req,res,next) =>{
+    const t=seq.transaction();
     try{
         const id=req.params.id;
-        const x=await Expense.findOne({where: {id:id}});
+        const x=await Expense.findOne({where: {id:id}},{transaction:t});
         req.user.totalExpense -= +x.amount;
-        req.user.save();
-        await Expense.destroy({where:{id:id}})
+        await req.user.save({transaction:t});
+        await Expense.destroy({where:{id:id}});
+
+        t.commit();
         res.status(200).json(null);
     }
     catch(err)
     {
+        if(t)
+        {
+            t.rollback();
+        }
         console.log(err);
     }
 }
