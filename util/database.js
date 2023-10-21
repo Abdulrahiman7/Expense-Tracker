91f31b54dc0e4fa62072cf788d477baf7650ce73
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-10-21 21:25:34 +0530

    final ready to deploy

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize('expense-tracker','root',null,{
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.DB_HOST,
     logging: false,
-    dialectOptions:{
-        password: 'Rockrolland@981'
-    }
 });
 
 module.exports=sequelize;
